bfe14bf936af841d1d377d17503ca4117770e33e
SPUCEEE <egle20161130@gmail.com>
2026-04-02 17:00:00 +0200

    started fixing navigations with arrows

--- Assets/Scripts/UIButtonHover.cs
+++ Assets/Scripts/UIButtonHover.cs
@@ -4,6 +4,8 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 public class UIButtonHover : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
 {
+    [SerializeField] private Button button;
+
     public Image BGImage;
     public Color normalColor = Color.black;
     public Color HoverlColor = Color.white;
@@ -11,7 +13,8 @@ public class UIButtonHover : MonoBehaviour, IPointerClickHandler, IPointerEnterH
     public TMP_Text btnText;
     public Color normalTextColor = Color.white;
     public Color HoverTextColor = Color.black;
-    public Color PressedTextColor = Color.red;
+	public Color selectedTextColor = Color.black;
+	public Color PressedTextColor = Color.red;
 
     void Start()
     {
@@ -20,7 +23,20 @@ public class UIButtonHover : MonoBehaviour, IPointerClickHandler, IPointerEnterH
             BGImage.color = normalColor;
     }
 
-    public void OnPointerEnter(PointerEventData eventData)
+    public void Selected(bool selected){
+        if (selected)
+        {
+            
+            btnText.color = selectedTextColor;
+        }
+        else
+        {
+            if (BGImage != null) BGImage.color = normalColor;
+            btnText.color = normalTextColor;
+		}
+
+	}
+	public void OnPointerEnter(PointerEventData eventData)
     {
         if (BGImage != null) BGImage.color = HoverlColor;
         btnText.color = HoverTextColor;
